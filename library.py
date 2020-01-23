a1700aa28457adcd94e6487c63840224125a89dc
Tie Bridgwood <tiebridgwood@Ties-MacBook-Pro.local>
2020-01-23 11:17:45 +0000

    docstrings

--- library.py
+++ library.py
@@ -9,14 +9,13 @@ from collections import Counter
 from collections import OrderedDict
 import string
 from keras import backend as K
+
+
+
 ### NLP Preprocessing ###
 
 '''
-Here we want to construct a complete nlp preprocessing pipeline so that we can take a corpus of documents and squeeze the real value out of it. 
-
-1. Stopwords - It is important to get rid of frequently occuring words like 'the' and 'a' that don't tell us very much about the nature of the document
-2. Stemming - Many words have very similar core meaning, but are different perhaps in their number or tense. Stemming will collapse such words into a single token
-3. Term Frequency / Inverse Document Frequency(TF-IDF) - In order to really understand the significance that a word holds in a document, we have to see how many times it comes up in a document, and then measure that against how many times that word comes up across all the docs in the corpus. Higher scores generally mean more meaningful words. 
+This part of the library is for preprocessing textual data. The entirety of this library is not used in the main notebook, but is there incase anyone should want to approach preprocessing or vectorization differently.
 '''
 
 sw = stopwords.words('english')
@@ -25,6 +24,17 @@ sw.remove('no')
 
 
 def remove_stopwords(doc: list or str, join: bool = False) -> list or str:
+    """
+    Removes overly frequent words that give less information about the document. Takes in either a string or list of strings and returns a string or a list of strings, depending on the ordering of text preprocessing.
+
+    Parameters:
+    doc (list or str): takes in individual documents either as full strings or list of strings in case tokenization has taken place first.
+
+    join (bool): defines whether or not the output should be a list of tokens or a joined string. 
+
+    Returns:
+    list or str: returns either a list of tokens or joined string depending on 'join' parameter.
+    """
     if not join:
         if isinstance(doc, list):
             filtered_tokens = [w for w in doc if w not in sw]
@@ -49,6 +59,15 @@ def remove_stopwords(doc: list or str, join: bool = False) -> list or str:
     
 
 def tf(document: str or list) -> dict:
+    """
+    Takes in a document and returns the relative frequencies of each word in the document.
+
+    Parameters:
+    document (str or list): the document to process
+
+    Returns:
+    dict: a dictionary with the words of the document as the keys and their relative frequencies as the values.
+    """
     if isinstance(document, str):
         document = document.split()
         
@@ -63,7 +82,17 @@ def tf(document: str or list) -> dict:
         bow[word] = count/len(document)
     return bow
 
-def idf(corpus: list) -> dict:
+def idf(corpus: list, verbose: bool = False) -> dict:
+    """
+    Calculates the inverse document frequencies for each word within a corpus. Inverse document frequency is the log of number of documents in the corpus divided by the number of documents in which the word in question appears. 
+
+    Parameters:
+    corpus (list): takes in a corpus in the form of a list of documents. Documents must be split/tokenized already. 
+    verbose (bool): on large corpora, idf can take a long time, the verbose option prints out how many documents have been processed. 
+
+    Returns:
+    dict: a dictionary of with the words as keys and their idf scores as their values.
+    """
     output = []
     vocab = set()
     for doc in corpus:
@@ -83,29 +112,50 @@ def idf(corpus: list) -> dict:
 
         vocabulary[word] = np.log((len(corpus)/float(doc_count)))
         f_counter += 1
-        if f_counter % 1000 == 1:
-            print(f'Number of words IDFed: {f_counter}')
+        if verbose:
+            if f_counter % 1000 == 1:
+                print(f'Number of words IDFed: {f_counter}')
         
     return vocabulary
 
-def tf_idf(stemmed_corpus: list) -> dict:
+def tf_idf(stemmed_corpus: list) -> list:
+    """
+    (WIP)
+    Caclulates the Term Frequency - Inverse Document Frequency for all documents in a corpus. TF-IDF is the relative term frequency of a word in a particularparticular document multiplied by its inverse document frequency for the corpus. 
+    
+    Parameters:
+    stemmed_corpus (list): A list of documents containing stemmed tokens. 
+
+    Returns:
+    list: Returns a list of dictionaries, each dictionary representing a document in the corpus with the tf_idf score for each word in the document. 
+    """
     output = []
     
     tfed = [tf(doc) for doc in stemmed_corpus]
     idfed = idf(stemmed_corpus)
     
-    doc_counter = 0
     for doc in tfed:
         for word in doc:
             doc[word] *= idfed[word]
         
-        doc_counter += 1
-#         if doc_counter % 1000 == 0:
-#             print(f"Vectorised {doc_counter} documents")
         
     return tfed
 
+### Data Handling ###
+
 def resample_majority(df: pd.DataFrame, criteria: str, majority_class: int) -> pd.DataFrame:
+    """
+    Reduces the size of a particular majority class in a dataframe in line with the mean of size of the remaining classes.
+
+    Parameters:
+    df (pd.DataFrame): The pandas dataframe to be downsampled.
+    criteria (str): The column in the dataframe to be downsampled.
+    majority_class (int): The class to be downsampled.
+
+
+    Returns:
+    pd.DataFrame: The resampled dataframe.
+    """
     majority_df = df[df[criteria] == majority_class]
     minority_df = df[df[criteria] != majority_class]
     minority_mean = int(minority_df[criteria].value_counts().mean())
